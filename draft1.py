b5d53afe095b899344b1617d40c41ba274166477
Enkh-Oyun <tsendayushenkhoyun@gmail.com>
2022-11-30 22:54:37 +0800

    zasalt

--- draft1.py
+++ draft1.py
@@ -3,6 +3,15 @@ import pygame
 pygame.init()
 
 screen_width = 800
-screen_height = 500
+screen_height = int(screen_width*0.8)
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption ("Santa Shooter")
+
+run = True
+while run:
+    for event in pygame.event.get:
+        if event.type == event.QUIT:
+            run = False
+pygame.quit()
+
